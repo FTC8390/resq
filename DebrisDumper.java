f10874e53b50fd01cc46d1acefa7481383668e9b
FTC8390 <ftc8390@gmail.com>
2015-12-01 18:34:42 -0500

    Create DebrisDumper.java

---
+++ DebrisDumper.java
@@ -0,0 +1,50 @@
+package ftc8390.resq;
+
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.util.Range;
+
+/**
+ * Created on 11/30/2015.
+ */
+public class DebrisDumper {
+  Servo servo;
+  boolean isRed;
+
+  double position=0.5;
+
+  double dumpValue, collectValue;
+
+  public void init(HardwareMap hwMap, boolean isRedIn) {
+    isRed = isRedIn;
+    if (isRed) {
+      servo = hwMap.servo.get("reddd");
+      dumpValue = 0.25;
+      collectValue = 0.75;
+    } else {
+      servo = hwMap.servo.get("bluedd");
+      dumpValue = 0.75;
+      collectValue = 0.25;
+    }
+
+    position = collectValue;
+    servo.setPosition(position);
+  }
+
+  public void start() {
+  }
+
+  public double changePositionBy(double amount) {
+    position = Range.clip(position + amount, 0, 1);
+    servo.setPosition(position);
+    return position;
+  }
+
+  public void dump() {
+
+  }
+
+  public void collect() {
+
+  }
+}
