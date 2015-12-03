09ae3d5f9b61e64a53b0649f9b2100a7b77a330e
Nerd Herd <ftc8390@gmail.com>
2015-12-03 14:53:19 -0500

    Update Teleop.java

--- Teleop.java
+++ Teleop.java
@@ -47,19 +47,24 @@ public class Teleop extends OpMode {
     if (gamepad1.dpad_down) {
       moosalot.driveTrain.lowSpeed();    //low speed
     }
+    
+    moosalot.drivetrain.tankDrive(gamepad1.left_stick_y, gamepad1.right_stick_y);
 
     if (gamepad2.left_bumper) {
       moosalot.raiseHookTowardMountain(); //scissor lift up
-    }
-
-    if (gamepad2.left_trigger > 0.75) {
+    } 
+    else if (gamepad2.left_trigger > 0.75) {
       moosalot.dropHookAndRetractLift();  //scissor lift down
+    } 
+    else {
+      moosalot.stopHook();
     }
-
-    moosalot.drivetrain.tankDrive(gamepad1.left_stick_y, gamepad1.right_stick_y);
       
     if(gamepad2.y){
       moosalot.winch.windUpMountain();
+    } 
+    else {
+      moosalot.winch.stop();
     }
     
     if (gamepad2.dpad_up){
@@ -72,9 +77,12 @@ public class Teleop extends OpMode {
     
     if (gamepad2.right_bumper){
       moosalot.whiskTrain.pullInDebris();    //whisk train in
-    }
-    
-    if (gamepad2.right_trigger > 0.75){
+    } 
+    else if (gamepad2.right_trigger > 0.75){
       moosalot.whiskTrain.pushOutDebris();
+    } 
+    else {
+      moosalot.whiskTrain.stop();
+    }
   }
 }
