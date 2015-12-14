de5f459779b0d4e4962f41e71daec341a26ce388
ftc8390 <ftc8390@gmail.com>
2015-12-14 09:23:52 -0500

    Update format

--- DriveTrain.java
+++ DriveTrain.java
@@ -9,73 +9,71 @@ import com.qualcomm.robotcore.util.Range;
  * Created on 11/30/2015.
  */
 public class DriveTrain {
-    public DcMotor leftDrive, rightDrive;
+  public DcMotor leftDrive, rightDrive;
 
-    boolean isFacingForward;
-    double speed;
+  boolean isFacingForward;
+  double speed;
 
-    public void init(HardwareMap hwMap) {
-        isFacingForward = true;
-        speed = 1.0;
+  public void init(HardwareMap hwMap) {
+    isFacingForward = true;
+    speed = 1.0;
 
-        leftDrive = hwMap.dcMotor.get("leftdrive");
-        rightDrive = hwMap.dcMotor.get("rightdrive");
-        rightDrive.setDirection(DcMotor.Direction.REVERSE);
-        // leftDrive.setDirection(DcMotor.Direction.REVERSE);
+    leftDrive = hwMap.dcMotor.get("leftdrive");
+    rightDrive = hwMap.dcMotor.get("rightdrive");
+    rightDrive.setDirection(DcMotor.Direction.REVERSE);
+    // leftDrive.setDirection(DcMotor.Direction.REVERSE);
 
+    leftDrive.setMode(DcMotorController.RunMode.RESET_ENCODERS);
+    rightDrive.setMode(DcMotorController.RunMode.RESET_ENCODERS);
+  }
 
-        leftDrive.setMode(DcMotorController.RunMode.RESET_ENCODERS);
-        rightDrive.setMode(DcMotorController.RunMode.RESET_ENCODERS);
+  public void setModeToRunUsingEncoders() {
+    leftDrive.setMode(DcMotorController.RunMode.RUN_USING_ENCODERS);
+    rightDrive.setMode(DcMotorController.RunMode.RUN_USING_ENCODERS);
+  }
+
+  public void start() {
+    leftDrive.setMode(DcMotorController.RunMode.RUN_WITHOUT_ENCODERS);
+    rightDrive.setMode(DcMotorController.RunMode.RUN_WITHOUT_ENCODERS);
+  }
+
+  public void runLeftAtPower(double power) {
+    leftDrive.setPower(Range.clip(power, -1, 1));
+  }
+
+  public void runRightAtPower(double power) {
+    rightDrive.setPower(Range.clip(power, -1, 1));
+  }
+
+  public void stop() {
+    leftDrive.setPower(0);
+    rightDrive.setPower(0);
+  }
+
+  public void tankDrive(double left, double right) {
+    if (isFacingForward == true) {
+      leftDrive.setPower(speed * left);
+      rightDrive.setPower(speed * right);
+    } else {
+      leftDrive.setPower(-speed * right);
+      rightDrive.setPower(-speed * left);
     }
-
-    public void setModeToRunUsingEncoders() {
-        leftDrive.setMode(DcMotorController.RunMode.RUN_USING_ENCODERS);
-        rightDrive.setMode(DcMotorController.RunMode.RUN_USING_ENCODERS);
-
-    }
-
-    public void start() {
-        leftDrive.setMode(DcMotorController.RunMode.RUN_WITHOUT_ENCODERS);
-        rightDrive.setMode(DcMotorController.RunMode.RUN_WITHOUT_ENCODERS);
-    }
-
-    public void runLeftAtPower(double power) {
-        leftDrive.setPower(Range.clip(power, -1, 1));
-    }
-
-    public void runRightAtPower(double power) {
-        rightDrive.setPower(Range.clip(power, -1, 1));
-    }
-
-    public void stop() {
-        leftDrive.setPower(0);
-        rightDrive.setPower(0);
-    }
-
-    public void tankDrive(double left, double right) {
-        if (isFacingForward == true) {
-            leftDrive.setPower(speed * left);
-            rightDrive.setPower(speed * right);
-        }else{
-            leftDrive.setPower(-speed * right);
-            rightDrive.setPower(-speed * left);
-        }
-    }
+  }
 
 
-    public void faceForward() {
-        isFacingForward = true;
-    }
+  public void faceForward() {
+    isFacingForward = true;
+  }
 
-    public void faceBackward() {
-        isFacingForward = false;
-    }
+  public void faceBackward() {
+    isFacingForward = false;
+  }
 
-    public void highSpeed() {
-        speed = 1;
-    }
+  public void highSpeed() {
+    speed = 1;
+  }
 
-    public void lowSpeed() {
-        speed = .2;
-    }
+  public void lowSpeed() {
+    speed = .2;
+  }
 }
