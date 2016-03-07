96ebb98d4fbaffb0fa2cbbde28ab96bb29f22b30
wgardnerg <wgardnerg@gmail.com>
2016-03-07 17:58:33 -0500

    .Added more captions to Timer     .Added an auto stop robot at the end of time, enless both back butons are pressed

--- Teleop.java
+++ Teleop.java
@@ -10,7 +10,7 @@ import com.qualcomm.robotcore.util.ElapsedTime;
 public class Teleop extends OpMode {
 
   private RobotResq moosalot;
-
+  Boolean stopAtEndOfMatch;
   private ElapsedTime runTime = new ElapsedTime();
   private Double timeLeft, runTimeDouble;
 
@@ -18,6 +18,7 @@ public class Teleop extends OpMode {
   public void init() {
     moosalot = new RobotResq();
     moosalot.init(hardwareMap);
+    stopAtEndOfMatch = true;
   }
 
   @Override
@@ -44,113 +45,122 @@ public class Teleop extends OpMode {
 
     if ((timeLeft <= 60) && (timeLeft > 40)) {
       telemetry.addData("B1: GO TO MOUNTAIN SOON!!", 0);
-      telemetry.addData("B2: PUT POKER OUT!!", 0);
+      telemetry.addData("B2: PUT POKER OUT WHEN YOU DO!!", 0);
     }
 
     if ((timeLeft <= 40) && (timeLeft >= 30)) {
       telemetry.addData("C: END GAME IN", String.format("%.2f", timeLeft - 30));
     }
+    if ((timeLeft <= 10) && (timeLeft >= 0)) {
+      telemetry.addData("C: END OF MATCH IN", String.format("%.2f", timeLeft));
+    }
 
+    if((gamepad1.back)&& (gamepad2.back)){
+      stopAtEndOfMatch = false;
+    }
 
-    if (gamepad2.b) {
-      //moosalot.redDebrisDumper.dump();
-      if (moosalot.redDebrisDumper.isDumped() == false) {
-        moosalot.redDebrisDumper.dumpSlowly();
+    if ((timeLeft == 0)&&(stopAtEndOfMatch)) {
+      moosalot.stop();
+    } else {
+      if (gamepad2.b) {
+        //moosalot.redDebrisDumper.dump();
+        if (moosalot.redDebrisDumper.isDumped() == false) {
+          moosalot.redDebrisDumper.dumpSlowly();
+        }
+      } else if (gamepad1.b) {
+        moosalot.redDebrisDumper.redHighDump();
+      } else {
+        moosalot.redDebrisDumper.collect();
       }
-    } else if (gamepad1.b) {
-      moosalot.redDebrisDumper.redHighDump();
-    } else {
-      moosalot.redDebrisDumper.collect();
-    }
 
 
-    if (gamepad2.x) {
-      //moosalot.blueDebrisDumper.dump();
-      if (moosalot.blueDebrisDumper.isDumped() == false) {
-        moosalot.blueDebrisDumper.dumpSlowly();
+      if (gamepad2.x) {
+        //moosalot.blueDebrisDumper.dump();
+        if (moosalot.blueDebrisDumper.isDumped() == false) {
+          moosalot.blueDebrisDumper.dumpSlowly();
+        }
+      } else if (gamepad1.x) {
+        moosalot.blueDebrisDumper.blueHighDump();
+      } else {
+        moosalot.blueDebrisDumper.collect();
       }
-    } else if (gamepad1.x) {
-      moosalot.blueDebrisDumper.blueHighDump();
-    } else {
-      moosalot.blueDebrisDumper.collect();
-    }
 
 
-    if (gamepad1.y) {
-      moosalot.driveTrain.faceForward();  //drive facing forward
-    }
+      if (gamepad1.y) {
+        moosalot.driveTrain.faceForward();  //drive facing forward
+      }
 
-    if (gamepad1.a) {
-      moosalot.driveTrain.faceBackward(); //drive facing backward
-    }
+      if (gamepad1.a) {
+        moosalot.driveTrain.faceBackward(); //drive facing backward
+      }
 
-    if (gamepad1.dpad_up) {
-      moosalot.driveTrain.highSpeed();   //high speed
-    }
+      if (gamepad1.dpad_up) {
+        moosalot.driveTrain.highSpeed();   //high speed
+      }
 
-    if (gamepad1.dpad_down) {
-      moosalot.driveTrain.lowSpeed();    //low speed
-    }
+      if (gamepad1.dpad_down) {
+        moosalot.driveTrain.lowSpeed();    //low speed
+      }
 
 
-    if (moosalot.churroHook.isRaised) {
-      moosalot.driveTrain.tankDrive(-gamepad1.left_stick_y, -gamepad1.right_stick_y);
-    } else {
-      moosalot.driveTrain.tankDriveBackwardsOnly(-gamepad1.left_stick_y, -gamepad1.right_stick_y);
-    }
+      if (moosalot.churroHook.isRaised) {
+        moosalot.driveTrain.tankDrive(-gamepad1.left_stick_y, -gamepad1.right_stick_y);
+      } else {
+        moosalot.driveTrain.tankDriveBackwardsOnly(-gamepad1.left_stick_y, -gamepad1.right_stick_y);
+      }
 
-    if (gamepad2.left_bumper) {
-      moosalot.raiseHookTowardMountain(); //scissor lift up
-    }
+      if (gamepad2.left_bumper) {
+        moosalot.raiseHookTowardMountain(); //scissor lift up
+      }
     /*else if (gamepad2.left_trigger > 0.75) {
       moosalot.dropHookAndRetractLift();  //scissor lift down
     } else {
       moosalot.stopHook();
     }*/
-    else {
-      if (gamepad2.left_stick_y < -0.75) {
-        moosalot.poker.pokeOut();
-      } else if (gamepad2.left_stick_y > +0.75) {
-        moosalot.poker.pullIn();
-      } else {
-        moosalot.poker.stop();
+      else {
+        if (gamepad2.left_stick_y < -0.75) {
+          moosalot.poker.pokeOut();
+        } else if (gamepad2.left_stick_y > +0.75) {
+          moosalot.poker.pullIn();
+        } else {
+          moosalot.poker.stop();
+        }
+
+        if (gamepad2.right_stick_y < -0.75) {
+          moosalot.lift.raise();
+        } else if (gamepad2.right_stick_y > +0.75) {
+          moosalot.lift.lower();
+        } else {
+          moosalot.lift.stop();
+        }
       }
 
-      if (gamepad2.right_stick_y < -0.75) {
-        moosalot.lift.raise();
-      } else if (gamepad2.right_stick_y > +0.75) {
-        moosalot.lift.lower();
+      if (gamepad2.y) {
+        moosalot.winch.wind();
       } else {
-        moosalot.lift.stop();
+        moosalot.winch.stop();
+      }
+
+      if (gamepad2.dpad_up || gamepad1.left_bumper) {
+        moosalot.churroHook.raise();
+      }
+
+      if (gamepad2.dpad_right) {
+        moosalot.churroHook.prepareForRamp();
+      }
+
+      if (gamepad2.dpad_down || (gamepad1.left_trigger > 0.75)) {
+        moosalot.churroHook.lower();
+      }
+
+      if (gamepad2.right_bumper) {
+        moosalot.whiskTrain.pushOutDebris();    //whisk train in
+      } else if (gamepad2.right_trigger > 0.75) {
+        moosalot.whiskTrain.pullInDebris();
+      } else {
+        moosalot.whiskTrain.stop();
       }
     }
-
-    if (gamepad2.y) {
-      moosalot.winch.wind();
-    } else {
-      moosalot.winch.stop();
-    }
-
-    if (gamepad2.dpad_up || gamepad1.left_bumper) {
-      moosalot.churroHook.raise();
-    }
-
-    if (gamepad2.dpad_right) {
-      moosalot.churroHook.prepareForRamp();
-    }
-
-    if (gamepad2.dpad_down || (gamepad1.left_trigger > 0.75)) {
-      moosalot.churroHook.lower();
-    }
-
-    if (gamepad2.right_bumper) {
-      moosalot.whiskTrain.pushOutDebris();    //whisk train in
-    } else if (gamepad2.right_trigger > 0.75) {
-      moosalot.whiskTrain.pullInDebris();
-    } else {
-      moosalot.whiskTrain.stop();
-    }
-
   }
 
 }
